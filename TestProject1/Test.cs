826a13366d2b0e59c0f5f6a61f79cdd1994a8d82
darioBD <dario.d.monte.d.o@gmail.com>
2024-04-11 14:17:06 -0300

    comit 1

--- TestProject1/Test.cs
+++ TestProject1/Test.cs
@@ -9,7 +9,7 @@ namespace TestProject1
         {
             ConcurrentStack<int> concurrentStack = new ConcurrentStack<int>();
 
-            int totalItems = 1000;
+            int totalItems = 30;
             int expectedItemCount = totalItems;
 
             // Agrega elementos a ConcurrentStack
@@ -34,7 +34,7 @@ namespace TestProject1
         {
             ConcurrentQueue<int> concurrentQueue = new ConcurrentQueue<int>();
 
-            int totalItems = 1000;
+            int totalItems = 30;
             int expectedItemCount = totalItems;
 
             // Agrega elementos a ConcurrentQueue
@@ -52,6 +52,51 @@ namespace TestProject1
             // Verifica que todos los elementos hayan sido eliminados
             Assert.Equal(0, concurrentQueue.Count);
         }
-    
+
+        [Fact]
+        public void TestSncronizacion()
+        {
+            ConcurrentStack<int> concurrentStack = new ConcurrentStack<int>();
+
+            int totalItems = 30;
+            int itemsAdded = 0;
+
+            // Agrega elementos a ConcurrentStack
+            for (int i = 0; i < totalItems; i++)
+            {
+                concurrentStack.Push(i);
+            }
+
+            // Realiza operaciones de lectura y escritura concurrentes
+            Parallel.Invoke(
+                () =>
+                {
+                    // Extrae elementos de ConcurrentStack
+                    int count = 0;
+                    while (concurrentStack.TryPop(out _))
+                    {
+                        count++;
+                    }
+                    Assert.Equal(totalItems, count);
+                },
+                () =>
+                {
+                    // Espera a que se completen las operaciones de extracción de elementos
+                    Thread.Sleep(100);
+
+                    // Agrega elementos adicionales a ConcurrentStack
+                    for (int i = 0; i < totalItems; i++)
+                    {
+                        concurrentStack.Push(i);
+                        Interlocked.Increment(ref itemsAdded);
+                    }
+                });
+
+            // Verifica que el recuento final sea correcto
+            Assert.Equal(totalItems, concurrentStack.Count);
+
+            // Verifica que todos los elementos hayan sido agregados correctamente
+            Assert.Equal(totalItems, itemsAdded);
+        }
     }
 }
